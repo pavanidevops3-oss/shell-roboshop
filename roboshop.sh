b3c94639a73094d2d86a76031deeddf151b1336f
pavani <pavanidevops3@gmail.com>
2025-10-24 08:42:31 -0400

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 ami-id="ami-09c813fb71547fc4f"
 sg_id="sg-0421c015ec6afe1d7"
+zone_id="Z0755238YFAMOXJZ58F2"
+domain_name="pm-cicd.fun"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.pm-cicd.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$domain_name" # pm-cicd.fun
+    fi
+
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $zone_id \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
